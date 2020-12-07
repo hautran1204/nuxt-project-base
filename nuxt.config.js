e5802d580181f4c0371e15eaa2e56b9386e24a2f
tran hoang thanh hau <hautran1204@gmail.com>
2020-12-08 00:22:19 +0700

    build project base

---
+++ nuxt.config.js
@@ -0,0 +1,58 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'nuxt-base',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+    '@/assets/scss/main.scss'
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    '@/plugins/axios'
+  ],
+
+  axios: {
+    baseURL: 'http://localhost:4000',
+  },
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    [
+      "@nuxtjs/dotenv",
+      {
+        path: ".env",
+        filename: process.env.NODE_ENV === "production" ? ".env.production" : ".env.local"
+      }
+    ],
+    '@nuxtjs/style-resources'
+  ],
+
+  styleResources: {
+    scss: [
+      '@/assets/scss/utils/*.scss'
+      ]
+  },
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    '@nuxtjs/axios'
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
